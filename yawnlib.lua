a6660065e48a4ec8c3256b2cda54438182e22396
yawn <68041302+yvwn@users.noreply.github.com>
2022-07-21 13:08:49 -0600

    Create yawnlib.lua

---
+++ yawnlib.lua
@@ -0,0 +1,19 @@
+local plr = game.Players.LocalPlayer
+local chr = plr.Character
+local hrp = chr.HumanoidRootPart
+        function  ttp(speed, pos)
+            TweenService = game:GetService("TweenService")
+tweenInfo = TweenInfo.new(speed)
+TweenGoal = {}
+TweenGoal.CFrame = pos
+PlayTween = TweenService:Create(hrp,tweenInfo,TweenGoal)
+PlayTween:Play()
+        end
+        
+        
+
+function tp(pos)
+    hrp.CFrame = pos
+end
+tp(CFrame.new(x, y, z))
+ttp(1,CFrame.new(x, y, z))
